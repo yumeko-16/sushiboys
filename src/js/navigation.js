5a5d8117aeff5d39f901c177808d2f3001fea0e6
sihoo's milk <>
2022-11-12 02:37:03 +0700

    refactor: Tidy up the navigation module.

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,43 +1,39 @@
 export const HumbergerToggle = (() => {
 
-  window.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html');
+  const header = document.querySelector('[data-header]');
+  const toggle = document.querySelector('[data-humberger-toggle]');
+  const links = document.querySelectorAll('[data-humberger-content] a');
+  const close = document.querySelector('[data-navigation-close]');
+  let scrollY = null;
 
-    const html = document.querySelector('html'),
-      header = document.querySelector('[data-header]'),
-      toggle = document.querySelector('[data-humberger-toggle]'),
-      links = document.querySelectorAll('[data-humberger-content] a'),
-      close = document.querySelector('[data-navigation-close]');
-    let scrollY = null;
+  if (toggle === null || links === null) { return }
 
-    if (toggle === null || links === null) { return }
+  toggle.addEventListener('change', (e) => {
+    if (e.target.checked === true) {
+      scrollY = window.pageYOffset;
+      setTimeout(() => {
+        html.classList.add('js-scroll-prevent');
+      }, 300);
+    } else {
+      html.classList.remove('js-scroll-prevent');
+      window.scrollTo(0, scrollY);
+      scrollY = null;
+      setTimeout(() => {
+        header.classList.remove('is-hide');
+      }, 100);
+    }
+  }, false);
 
-    toggle.addEventListener('change', (e) => {
-      if (e.target.checked === true) {
-        scrollY = window.pageYOffset;
-        setTimeout(() => {
-          html.classList.add('js-scroll-prevent');
-        }, 300);
+  [...links].map((link) => {
+    link.addEventListener('click', (e) => {
+      if (e.target.href.includes('#')) {
+        toggle.checked = false;
+        html.classList.remove('js-scroll-prevent');
       } else {
         html.classList.remove('js-scroll-prevent');
-        window.scrollTo(0, scrollY);
-        scrollY = null;
-        setTimeout(() => {
-          header.classList.remove('js-hide');
-        }, 100);
       }
     }, false);
-
-    [...links].map((link) => {
-      link.addEventListener('click', (e) => {
-        if (e.target.href.includes('#')) {
-          toggle.checked = false;
-          html.classList.remove('js-scroll-prevent');
-        } else {
-          html.classList.remove('js-scroll-prevent');
-        }
-      }, false);
-    });
-
-  }, false);
+  });
 
 })();
