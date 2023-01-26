56b78293dde61f97396b1e24f28daeff47c0e427
sihoo's milk <>
2023-01-26 23:13:31 +0700

    fix: Fixed an issue that exported a nonexistent module.

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,2 @@
 import { ToggleHeader } from 'js/header';
 import { HumbergerToggle } from 'js/navigation';
-import { Navigation } from 'js/navigation';
-// import { Validate, Post } from 'js/contact';
