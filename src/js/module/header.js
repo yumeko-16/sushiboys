a412c353a537df5e6658d66471f2d2b1b2fce86a
tomohiro.maeno <>
2023-08-14 00:07:47 +0700

    refactor: Refactor header toggle functionality.

--- src/js/module/header.js
+++ src/js/module/header.js
@@ -1,20 +1,19 @@
 export const ToggleHeader = (() => {
   const header = document.getElementById('header');
-  let positionY = 0;
+  let lastScrollY = 0;
 
   if (!header) {
     return;
   }
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY < positionY) {
+  const updateHeaderVisibility = () => {
+    if (window.scrollY < lastScrollY || window.scrollY <= 10) {
       header.classList.remove('is-hide');
     } else {
       header.classList.add('is-hide');
     }
-    positionY = window.scrollY;
-    if (positionY <= 10) {
-      header.classList.remove('is-hide');
-    }
-  });
+    lastScrollY = window.scrollY;
+  }
+
+  window.addEventListener('scroll', updateHeaderVisibility);
 })();
