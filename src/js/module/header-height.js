6fbdcf672b13198da911b453d10f2e44ad4fa86b
tomohiro.maeno <>
2024-02-24 19:38:11 +0700

    hotfix: Fixed the failure to add space to heroes on the homepage.

--- src/js/module/header-height.js
+++ src/js/module/header-height.js
@@ -3,12 +3,16 @@ export const HeightOfHeader = (() => {
   const heightOfHeader = () => {
     const WRAPPER = document.getElementById('wrapper');
     const HEADER = document.getElementById('header');
-    const HEADER_HEIGHT = HEADER.clientHeight;
     const HERO = document.getElementById('hero');
-    console.log(HERO);
+
+    if (!WRAPPER || !HEADER || !HERO) {
+      return;
+    }
+
+    const HEADER_HEIGHT = HEADER.clientHeight;
 
     WRAPPER.style.paddingBlockStart = `${HEADER_HEIGHT}px`;
-    HERO != null ? (HERO.style.marginBlockStart = `-${HEADER_HEIGHT}px`) : 0;
+    HERO.style.marginBlockStart = `-${HEADER_HEIGHT}px`;
   };
 
   document.addEventListener('DOMContentLoaded', heightOfHeader);
