9f7953fc1df84828cb8f4244802addb3068febcf
tomohiro.maeno <>
2024-03-02 23:49:51 +0700

    change: Stop fixed headers.

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,5 +1,1 @@
-import { HeightOfHeader } from './module/header-height';
-// import { HeightOfFooter } from './module/footer-height';
-import { ToggleHeader } from './module/header';
 import { HumbergerToggle } from './module/navigation';
-// import { Validation, Submit } from './module/contact';
