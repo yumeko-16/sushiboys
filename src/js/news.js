1184e48311a4fd8079c79e47b45f74d89a5843b1
tomohiro.maeno <>
2024-03-03 00:53:28 +0700

    change: Stop fixed headers.

--- src/js/news.js
+++ src/js/news.js
@@ -1,4 +1,1 @@
-// import { HeightOfHeader } from './module/header-height';
-// import { HeightOfFooter } from './module/footer-height';
-// import { ToggleHeader } from './module/header';
 import { HumbergerToggle } from './module/navigation';
